b2968cff8ed2842c3a366e283e456222a93b4735
Chris Adams <chris@productscience.co.uk>
2018-04-07 23:20:26 +0200

    Use a yield to fetch the data

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -12,11 +12,11 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
             
-
+            
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
-                text = quote.css("span.text::text").extract_first(),
-                author = quote.css('small.author::text').extract_first(),
-                tags = quote.css('div.tags a.tag::text').extract()
+                'text' : quote.css("span.text::text").extract_first(),
+                'author' : quote.css('small.author::text').extract_first(),
+                'tags' : quote.css('div.tags a.tag::text').extract()
             }
