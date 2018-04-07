67a17555a02e73d8692a06c35e77cb017e553ad6
Chris Adams <chris@productscience.co.uk>
2018-04-07 23:12:04 +0200

    Yield instead

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -11,10 +11,12 @@ class QuotesSpider(scrapy.Spider):
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
+            
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                text = quote.css("span.text::text").extract_first(),
+                author = quote.css('small.author::text').extract_first(),
+                tags = quote.css('div.tags a.tag::text').extract()
+            }
