88384f2b4b94da383cdb1497a06a0cd17a790ce9
Chris Adams <chris@productscience.co.uk>
2018-04-07 23:28:41 +0200

    Switch to showing how the following works for a scraper

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -6,13 +6,12 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://quotes.toscrape.com/page/1/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
-            
-            
+
+
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
@@ -20,3 +19,9 @@ class QuotesSpider(scrapy.Spider):
                 'author' : quote.css('small.author::text').extract_first(),
                 'tags' : quote.css('div.tags a.tag::text').extract()
             }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
