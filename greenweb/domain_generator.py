140456884c3c1c2380928ba978ad6efaa86f0c1f
Chris Adams <chris@productscience.co.uk>
2018-04-12 11:55:36 +0200

    Add utility functions to slice make smaller sections for scraper

--- greenweb/domain_generator.py
+++ greenweb/domain_generator.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import zipfile
+import requests
 
 # Read a csv file, and return a generator for a spider to iterate through.
 def gen_urls(file_object):
@@ -27,10 +29,43 @@ def cut_down_list(no_of_urls, file_path):
 
         subection_filepath = "top-{}-subsection.csv".format(no_of_urls)
         with open(subection_filepath, 'w+') as subsection_file:
-            
-                for idx, line in enumerate(file):
-                    if idx < no_of_urls:
-                        subsection_file.write(line)
+
+            for idx, line in enumerate(file):
+                if idx < no_of_urls:
+                    subsection_file.write(line)
 
 
     return subection_filepath
+
+def fetch_top_mil():
+    """
+    Fetch the compressed top million urls file from AWS bucket, and decompresses it
+    
+    Returns a filepath that can be opened
+    """
+
+    file_path = 'top-1m.csv.zip'
+
+    req = requests.get(
+        'http://s3.amazonaws.com/alexa-static/{}'.format(file_path), stream=True)
+
+    if req.status_code == 200:
+
+        with open(file_path, 'wb') as fd:
+            for chunk in req.iter_content(chunk_size=128):
+                fd.write(chunk)
+
+        return file_path
+
+    # otherwise assume it failed
+    return False
+
+
+def decompress_top_mil(file_path):
+    """
+    Decompress the compressed file passed in. Assumes it's a zip file.
+    """
+
+    zip_ref = zipfile.ZipFile(file_path, 'r')
+    zip_ref.extractall('.')
+    zip_ref.close()
--- greenweb/test_domain_generator.py
+++ greenweb/test_domain_generator.py
@@ -1,5 +1,6 @@
+import os
 import pytest
-from domain_generator import gen_urls, cut_down_list
+from domain_generator import gen_urls, cut_down_list, fetch_top_mil, decompress_top_mil
 
 def test_we_return_domains_with_no_trailing_chars():
     with open('top-50.csv') as csvfile:
@@ -21,3 +22,24 @@ def test_we_make_subections_of_file():
 
     with open(subsection_filepath) as subsection_file:
         assert len(subsection_file.readlines()) == 5
+
+
+@pytest.mark.slowtest
+def test_we_can_fetch_top_mil():
+
+    assert os.path.exists('top-1m.csv.zip') == False
+
+    res = fetch_top_mil()
+
+    assert res == 'top-1m.csv.zip'
+    assert os.path.exists('top-1m.csv.zip') == True
+
+@pytest.mark.slowtest
+def test_decompress_top_mil():
+        
+    fetch_top_mil()
+    assert os.path.exists('top-1m.csv.zip') == True
+
+    decompress_top_mil('top-1m.csv.zip')
+
+    assert os.path.exists('top-1m.csv') == True
