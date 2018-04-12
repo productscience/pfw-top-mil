f9c44317bb8a4d80c7bf54887df1231a5e87250c
Chris Adams <chris@productscience.co.uk>
2018-04-12 23:59:53 +0200

    Got the spider working locally

--- greenweb/greenweb/spiders/green_or_not_spider.py
+++ greenweb/greenweb/spiders/green_or_not_spider.py
@@ -1,23 +1,28 @@
 import json
 import scrapy
+from pathlib import Path
 
 
-# import ipdb; ipdb.set_trace()
-
-from greenweb.domain_generator import gen_urls
+from greenweb.domain_generator import (gen_urls, fetch_top_mil,
+                                       decompress_top_mil, cut_down_list)
 
 
 class GreenOrNotSpider(scrapy.Spider):
     name = "green_or_not"
 
     def start_requests(self):
-        
+
         tag = getattr(self, 'tag', None)
+        assert type(tag) is not None
         # exit early if we don't have an integer as our tag
-        assert type(tag) == int
+        assert int(tag)
 
         file_path = 'top-{}-subsection.csv'.format(tag)
 
+        if not Path(file_path).exists():
+            fetch_top_mil()
+            decompress_top_mil('top-1m.csv.zip')
+            cut_down_list(int(tag), 'top-1m.csv')
 
         with open(file_path) as csvfile:
 
